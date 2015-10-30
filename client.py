b8d4fa352e9938a27a43752cd18ac8f92d5759d1
Nikita Marchant <nikita.marchant@gmail.com>
2015-10-30 13:03:51 +0100

    Fix header print

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ url = 'http://localhost:5000'
 @asyncio.coroutine
 def fetch(url):
     response = yield from aiohttp.post(url)
-    print("Content-encoding:", response.headers)
+    print("content-encoding:", response.headers['content-encoding'])
     return (yield from response.release())
 
 content = asyncio.get_event_loop().run_until_complete(
